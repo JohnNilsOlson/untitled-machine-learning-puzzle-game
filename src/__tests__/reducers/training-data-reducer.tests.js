eaf63d99168f5dd8a5f667262ee1c6be4285603f
JohnNils Olson <johnnils@gmail.com>
2020-09-25 21:01:14 -0700

    add failing test for training data add data

--- src/__tests__/reducers/training-data-reducer.tests.js
+++ src/__tests__/reducers/training-data-reducer.tests.js
@@ -1,7 +1,30 @@
 import trainingDataReducer from '../../reducers/training-data-reducer';
 
 describe('trainingDataReducer', () => {
+
+  let action;
+
   test('Should return default state if no action type is passed into the reducer', () => {
     expect(trainingDataReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should add training data', () => {
+    const input = [1,2]
+    const output = 2
+    const id = 1
+    action = {
+      type: 'ADD_DATA',
+      input: input,
+      output: output,
+      id: id
+    }
+    expect(trainingDataReducer({}, action)).toEqual({
+      [id] : {
+        input: [1,2],
+        output: 2,
+        id: id 
+      }
+    });
+  });
+  
 });
