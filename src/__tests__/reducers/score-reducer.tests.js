005faebc122d4cf4f96c824871bd61289e1bba95
JohnNils Olson <johnnils@gmail.com>
2020-10-02 16:30:44 -0700

    add failing test for increment player score

--- src/__tests__/reducers/score-reducer.tests.js
+++ src/__tests__/reducers/score-reducer.tests.js
@@ -2,7 +2,22 @@ import scoreReducer from '../../reducers/score-reducer';
 
 describe('scoreReducer', () => {
 
+  const defaultState = {
+    playerScore: 0,
+    AIScore: 0
+  }
+
   test('Should return default state if no action type is passed into the reducer', () => {
     expect(scoreReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should increment player score by 1', () => {
+    const action = {
+      type: 'INCREMENT_PLAYER'
+    }
+    expect(scoreReducer(defaultState, action)).toEqual({
+      playerScore: 1,
+      AIScore: 0
+    });
   });
 });
