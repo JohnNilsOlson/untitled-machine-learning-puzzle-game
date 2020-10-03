e3be92ebfd7ef9c8ea007069f5c189e8dc5d6ed6
JohnNils Olson <johnnils@gmail.com>
2020-10-03 14:56:27 -0700

    add combined rootReducer to redux store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducer from './reducers/training-data-reducer';
+import rootReducer from './reducers/index';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
