957c4d1837fe986b9018416ef84b648ffe0ff47e
JohnNils Olson <johnnils@gmail.com>
2020-10-05 11:28:22 -0700

    added ai reducer to combined root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import trainingDataReducer from './training-data-reducer';
 import playerScoreReducer from './player-score-reducer';
 import AIScoreReducer from './ai-score-reducer';
+import AIReducer from './ai-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   trainingData: trainingDataReducer,
   playerScore: playerScoreReducer,
-  AIScore: AIScoreReducer
+  AIScore: AIScoreReducer,
+  AI: AIReducer
 });
 
 export default rootReducer;
