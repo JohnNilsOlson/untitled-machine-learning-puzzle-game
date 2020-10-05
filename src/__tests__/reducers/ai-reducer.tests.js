29124e0660e41745c94a9ac880dfa4a138abc659
JohnNils Olson <johnnils@gmail.com>
2020-10-05 09:32:43 -0700

    add failing test for ai train action

--- src/__tests__/reducers/ai-reducer.tests.js
+++ src/__tests__/reducers/ai-reducer.tests.js
@@ -1,8 +1,22 @@
 import AIReducer from '../../reducers/ai-reducer';
+import brain from 'brain.js';
 
 describe('AIReducer', () => {
 
   test('Should return default state if no action type is passed into the reducer', () => {
     expect(AIReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully train ai', () => {
+    const defaultState = new brain.recurrent.RNN();
+    const trainingData = [
+      {input: [1,2], output: 1}
+    ]
+    const action = {
+      type: 'TRAIN_AI',
+      trainingData: trainingData
+    }
+    expect(AIReducer(defaultState, action)).toHaveProperty('error');
+    expect(AIReducer(defaultState, action)).toHaveProperty('iterations');
   });
 });
