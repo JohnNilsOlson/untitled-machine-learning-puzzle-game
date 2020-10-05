6aa95cd53354109ad039a7597c3079a4fda43c57
JohnNils Olson <johnnils@gmail.com>
2020-10-05 12:12:06 -0700

    add code to pass user pattern reducer add input test

--- src/reducers/user-pattern-reducer.js
+++ src/reducers/user-pattern-reducer.js
@@ -1,5 +1,11 @@
 const defaultState = []
 
 export default (state = defaultState, action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_INPUT':
+      const { input } = action;
+      return [...state, input];
+    default:
+      return state;
+  }
 }
