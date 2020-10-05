f02cba674d97fc60b8b24912f53afbe7a9a3f757
JohnNils Olson <johnnils@gmail.com>
2020-10-05 11:01:56 -0700

    add code to pass train ai test

--- src/reducers/ai-reducer.js
+++ src/reducers/ai-reducer.js
@@ -1,3 +1,15 @@
-export default (state = {}, action) => {
-  return state;
+import brain from 'brain.js';
+
+const defaultState = new brain.recurrent.RNN();
+
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    case 'TRAIN_AI':
+      const { trainingData } = action;
+      const newState = state;
+      newState.train(trainingData);
+      return newState;
+    default:
+      return state;
+  }
 }
