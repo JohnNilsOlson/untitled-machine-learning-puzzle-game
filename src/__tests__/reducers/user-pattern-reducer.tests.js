a7c459c49d918614a7a31914608dba3ddcdedd47
JohnNils Olson <johnnils@gmail.com>
2020-10-05 12:09:28 -0700

    add failing test for user pattern reducer add input action

--- src/__tests__/reducers/user-pattern-reducer.tests.js
+++ src/__tests__/reducers/user-pattern-reducer.tests.js
@@ -2,7 +2,18 @@ import userPatternReducer from '../../reducers/user-pattern-reducer';
 
 describe('user pattern reducer', () => {
 
+  const defaultState = [];
+
   test('Should return default state if no action type is passed into the reducer', () => {
     expect(userPatternReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should correctly add value to user pattern array', () => {
+    const input = 1;
+    const action = {
+      type: 'ADD_INPUT',
+      input: input
+    }
+    expect(userPatternReducer(defaultState, action)).toEqual([1])
   });
 });
