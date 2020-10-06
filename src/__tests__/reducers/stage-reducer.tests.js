55f2da2e9a7f5ed480c03cef29c320c8bdcacf2c
JohnNils Olson <johnnils@gmail.com>
2020-10-06 16:24:01 -0700

    add failing test for increment stage action

--- src/__tests__/reducers/stage-reducer.tests.js
+++ src/__tests__/reducers/stage-reducer.tests.js
@@ -2,7 +2,17 @@ import stageReducer from '../../reducers/stage-reducer';
 import * as c from '../../actions/ActionTypes';
 
 describe('stageReducer', () => {
+
+  let defaultState = 1;
+
   test('Should return default state if no action type is passed into the reducer', () => {
     expect(stageReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should increment stage by 1', () => {
+    const action = {
+      type: 'INCREMENT_STAGE'
+    }
+    expect(stageReducer(defaultState, action)).toEqual(2);
   });
 });
