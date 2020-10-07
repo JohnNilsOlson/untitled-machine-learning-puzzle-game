e5765e717f7c75baf2f3086c2e97c315115a4d9d
JohnNils Olson <johnnils@gmail.com>
2020-10-06 22:00:49 -0700

    add functionality for user defining rules of rock paper scissors

--- src/components/RPSStageOne.js
+++ src/components/RPSStageOne.js
@@ -1,11 +1,58 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { incrementStage, addTrainingData } from '../actions';
 
-function RPSStageOne(props) {
-  return (
-    <React.Fragment>
-      <h1>Rock, Paper, Scissors - Stage 1</h1>
-    </React.Fragment>
-  );
-};
+import Button from 'react-bootstrap/Button';
 
-export default RPSStageOne;
+class RPSStageOne extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rock: true,
+      paper: false,
+      scissors: false
+    }
+  }
+
+  handleAnswerClick = (input) => {
+    if (this.state.rock === true && input === 'paper') {
+      this.setState({rock: false, paper: true});
+    } else if (this.state.paper === true && input === 'scissors') {
+      this.setState({paper: false, scissors: true});
+    } else if (this.state.scissors === true && input === 'rock') {
+      this.setState({scissors: false});
+    } else {
+      alert('Incorrect!');
+    }
+  }
+
+  render() {
+    let currentStep; 
+    if (this.state.rock === true) {
+      currentStep = 'What beats rock?'
+    } else if (this.state.paper === true) {
+      currentStep = 'What beats paper?'
+    } else if (this.state.scissors === true) {
+      currentStep = 'What beats scissors?'
+    }
+    return (
+      <React.Fragment>
+        <h1>Rock, Paper, Scissors - Stage 1</h1>
+        <h3>Define the rules of Rock, Paper, Scissors and teach them to your AI</h3>
+        <h3>{currentStep}</h3>
+        <Button variant='outline-dark' onClick={()=>this.handleAnswerClick('rock')}>Rock</Button>
+        <Button variant='outline-dark' onClick={()=>this.handleAnswerClick('paper')}>Paper</Button>
+        <Button variant='outline-dark' onClick={()=>this.handleAnswerClick('scissors')}>Scissors</Button>
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    stage: state.stage,
+    trainingData: state.trainingData
+  }
+}
+
+export default connect(mapStateToProps)(RPSStageOne);
