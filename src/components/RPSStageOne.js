fc7e36dc3d81d8210a045f23e6c34b2ff2cd3336
JohnNils Olson <johnnils@gmail.com>
2020-10-06 21:14:15 -0700

    rename rock paper scissors component to split into stages

---
+++ src/components/RPSStageOne.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RPSStageOne(props) {
+  return (
+    <React.Fragment>
+      <h1>Rock, Paper, Scissors - Stage 1</h1>
+    </React.Fragment>
+  );
+};
+
+export default RPSStageOne;
