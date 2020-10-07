8db32da057c5f4a68c3d6d748f48de75dc678b64
JohnNils Olson <johnnils@gmail.com>
2020-10-07 08:50:21 -0700

    update to remove class, use hooks for local state

--- src/components/RPSStageOne.js
+++ src/components/RPSStageOne.js
@@ -1,69 +1,65 @@
 import React from 'react';
+import { useState } from 'react';
 import { connect } from 'react-redux';
 import { incrementStage, addTrainingData } from '../actions';
 
 import Button from 'react-bootstrap/Button';
 
-class RPSStageOne extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rock: true,
-      paper: false,
-      scissors: false
-    }
-  }
+function RPSStageOne(props) {
+  const  [ rock, setRock ]  = useState(true);
+  const [ paper, setPaper ] = useState(false);
+  const  [scissors, setScissors ] = useState(false);
 
-  handleAnswerClick = (input) => {
-    const { dispatch } = this.props;
-    if (this.state.rock === true && input === 'paper') {
-      this.setState({rock: false, paper: true});
+  const handleAnswerClick = (input) => {
+    const { dispatch } = props;
+    if (rock === true && input === 'paper') {
+      setRock(false);
+      setPaper(true);
       dispatch(addTrainingData(1,2));
-    } else if (this.state.paper === true && input === 'scissors') {
-      this.setState({paper: false, scissors: true});
+    } else if (paper === true && input === 'scissors') {
+      setPaper(false);
+      setScissors(true);
       dispatch(addTrainingData(2,3));
-    } else if (this.state.scissors === true && input === 'rock') {
-      this.setState({scissors: false});
+    } else if (scissors === true && input === 'rock') {
+      setScissors(false);
       dispatch(addTrainingData(3,1));
     } else {
       alert('Incorrect!');
     }
   }
 
-  stageChange = () => {
-    const { dispatch } = this.props;
+  const stageChange = () => {
+    const { dispatch } = props;
     dispatch(incrementStage());
   }
 
-  render() {
-    let currentStep; 
-    if (this.state.rock === true) {
-      currentStep = 'What beats rock?'
-    } else if (this.state.paper === true) {
-      currentStep = 'What beats paper?'
-    } else if (this.state.scissors === true) {
-      currentStep = 'What beats scissors?'
-    }
-    if (this.state.rock === false && this.state.paper === false && this.state.scissors === false) {
-      return (
-        <React.Fragment>
-          <h1>Rock, Paper, Scissors - Stage 1</h1>
-          <h3>Well done!</h3>
-          <Button variant='outline-dark' onClick={()=>this.stageChange()}>Train</Button>
-        </React.Fragment>
-      )
-    } else {
-      return (
-        <React.Fragment>
-          <h1>Rock, Paper, Scissors - Stage 1</h1>
-          <h3>Define the rules of Rock, Paper, Scissors and teach them to your AI</h3>
-          <h3>{currentStep}</h3>
-          <Button variant='outline-dark' onClick={()=>this.handleAnswerClick('rock')}>Rock</Button>
-          <Button variant='outline-dark' onClick={()=>this.handleAnswerClick('paper')}>Paper</Button>
-          <Button variant='outline-dark' onClick={()=>this.handleAnswerClick('scissors')}>Scissors</Button>
-        </React.Fragment>
-      );
-    }
+  let currentStep; 
+  if (rock === true) {
+    currentStep = 'What beats rock?'
+  } else if (paper === true) {
+    currentStep = 'What beats paper?'
+  } else if (scissors === true) {
+    currentStep = 'What beats scissors?'
+  }
+  if (rock === false && paper === false && scissors === false) {
+    return (
+      <React.Fragment>
+        <h1>Rock, Paper, Scissors - Stage 1</h1>
+        <h3>Well done!</h3>
+        <Button variant='outline-dark' onClick={()=>stageChange()}>Train</Button>
+      </React.Fragment>
+    )
+  } else {
+    return (
+      <React.Fragment>
+        <h1>Rock, Paper, Scissors - Stage 1</h1>
+        <h3>Define the rules of Rock, Paper, Scissors and teach them to your AI</h3>
+        <h3>{currentStep}</h3>
+        <Button variant='outline-dark' onClick={()=>handleAnswerClick('rock')}>Rock</Button>
+        <Button variant='outline-dark' onClick={()=>handleAnswerClick('paper')}>Paper</Button>
+        <Button variant='outline-dark' onClick={()=>handleAnswerClick('scissors')}>Scissors</Button>
+      </React.Fragment>
+    );
   }
 }
 
