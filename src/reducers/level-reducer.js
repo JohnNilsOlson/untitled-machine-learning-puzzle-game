712caf2a2af0a46829c72bc3683cd83761c56d09
JohnNils Olson <johnnils@gmail.com>
2020-10-09 08:54:08 -0700

    add code to pass level reducer level change action

--- src/reducers/level-reducer.js
+++ src/reducers/level-reducer.js
@@ -1,3 +1,12 @@
-export default (state={}, action) => {
-  return state;
+const defaultState = 0;
+
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    case 'LEVEL_CHANGE':
+      const { level } = action;
+      const newState = state + level;
+      return newState;
+    default:
+      return state;
+  }
 }
