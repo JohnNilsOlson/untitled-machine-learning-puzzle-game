b73cfefa0a31c9f37bff76f0fe1ee2290aaf794b
JohnNils Olson <johnnils@gmail.com>
2020-10-09 08:48:52 -0700

    add failing test for level reducer level change action

--- src/__tests__/reducers/level-reducer.tests.js
+++ src/__tests__/reducers/level-reducer.tests.js
@@ -2,7 +2,18 @@ import levelReducer from '../../reducers/level-reducer';
 
 describe('stageReducer', () => {
 
+  const defaultState = 0;
+
   test('Should return default state if no action type is passed into the reducer', () => {
     expect(levelReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should change level', () => {
+    const level = 1;
+    const action = {
+      type: 'LEVEL_CHANGE',
+      level: level
+    }
+    expect(levelReducer(defaultState, action)).toEqual(1);
   });
 });
