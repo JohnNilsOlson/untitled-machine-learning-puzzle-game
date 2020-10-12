c67f6efeedb858086cae322dd2c564a02469f46e
JohnNils Olson <johnnils@gmail.com>
2020-10-12 15:51:50 -0700

    add code to pass view reducer learn action test

--- src/reducers/view-reducer.js
+++ src/reducers/view-reducer.js
@@ -2,6 +2,8 @@ export default (state = {}, action) => {
   switch (action.type) {
     case 'PLAY':
       return 'play';
+    case 'LEARN':
+      return 'learn';
     default:
       return state;
   }
