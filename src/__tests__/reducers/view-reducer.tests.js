a70d856e40d6affeb23a8dc0f62b1853264acf1b
JohnNils Olson <johnnils@gmail.com>
2020-10-12 15:46:58 -0700

    add failing test for view reducer play action

--- src/__tests__/reducers/view-reducer.tests.js
+++ src/__tests__/reducers/view-reducer.tests.js
@@ -4,4 +4,11 @@ describe('viewReducer', () => {
   test('Should return default state if no action type is passed into the reducer', () => {
     expect(viewReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should change view', () => {
+    const action = {
+      type: 'PLAY'
+    }
+    expect(viewReducer({}, action)).toEqual('play');
+  });
 });
